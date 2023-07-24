27cc09bfca41e0e04fe52ed3608a4a3cb556a7cb
Junko011 <140358686+Junko011@users.noreply.github.com>
2023-07-23 19:22:45 -0600

    Base de la pagina web
    En estos archivos estan 3 que son las bases fundamentales de la página web, uno es un archivo HTML, otro es CSS y el ultimo es de JavaScript y cada uno de ellos tiene una base simple

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript para interacciones en la página (opcional)
+// Por ejemplo, podrías agregar un saludo animado al hacer clic en el título.
+const header = document.querySelector('header');
+header.addEventListener('click', () => {
+  alert('¡Hola! Gracias por visitar mi página.');
+});
